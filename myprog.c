bd64834332bb1b679c111935316cc320a6287cda
Rndmun <91401175+Rndmun@users.noreply.github.com>
2024-02-17 05:35:30 -0800

    Question 3

--- myprog.c
+++ myprog.c
@@ -34,3 +34,30 @@
 
 //     return 0;
 // }
+
+
+//Question 03
+#include <stdio.h>
+
+int main() {
+    for (int n = 1; n <= 100; n++) {
+        if (n % 2 == 0 && n % 3 == 0 && n % 5 == 0) {
+            printf("BishBashBosh\n");
+        } else if (n % 2 == 0 && n % 3 == 0) {
+            printf("BishBash\n");
+        } else if (n % 2 == 0 && n % 5 == 0) {
+            printf("BishBosh\n");
+        } else if (n % 3 == 0 && n % 5 == 0) {
+            printf("BashBosh\n");
+        } else if (n % 2 == 0) {
+            printf("Bish\n");
+        } else if (n % 3 == 0) {
+            printf("Bash\n");
+        } else if (n % 5 == 0) {
+            printf("Bosh\n");
+        } else {
+            printf("%d\n", n);
+        }
+    }
+    return 0;
+}
