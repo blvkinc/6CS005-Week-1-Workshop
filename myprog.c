1c2efbbc72abda3886efa2a6104af91fc672ffaa
Rndmun <91401175+Rndmun@users.noreply.github.com>
2024-02-17 05:29:30 -0800

    Question 1

--- myprog.c
+++ myprog.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <name> <age>\n", argv[0]);
-        return 1;
-    }
 
-    char *name = argv[1];
-    int age = atoi(argv[2]);
+//Question 01
+// int main(int argc, char *argv[]) {
+//     if (argc != 3) {
+//         printf("Usage: %s <name> <age>\n", argv[0]);
+//         return 1;
+//     }
 
-    printf("Hello %s, you are %d years old.\n", name, age);
+//     char *name = argv[1];
+//     int age = atoi(argv[2]);
 
-    return 0;
-}
+//     printf("Hello %s, you are %d years old.\n", name, age);
+
+//     return 0;
+// }
