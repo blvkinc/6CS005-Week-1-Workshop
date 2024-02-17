4dcf7e620edc690182b6210be32feaa077311e0c
Rndmun <91401175+Rndmun@users.noreply.github.com>
2024-02-17 05:28:47 -0800

    first commit

---
+++ myprog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <name> <age>\n", argv[0]);
+        return 1;
+    }
+
+    char *name = argv[1];
+    int age = atoi(argv[2]);
+
+    printf("Hello %s, you are %d years old.\n", name, age);
+
+    return 0;
+}
